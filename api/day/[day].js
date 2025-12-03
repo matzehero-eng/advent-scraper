6f14e8a71ec5b7f1e400837a62bba0c727109933
matzehero-eng <matze.hero@gmail.com>
2025-12-03 23:20:30 +0100

    Update [day].js

--- api/day/[day].js
+++ api/day/[day].js
@@ -1,6 +1,18 @@
 import { load } from "cheerio";
 
 export default async function handler(req, res) {
+
+  
+// --- CORS FIX ---
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  // --- ENDE CORS FIX ---
+  
   try {
     const { day } = req.query;
 
